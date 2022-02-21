aa23dd0afd38ba476d2da7aa76b49845062a1593
patricktunez123 <tunezepatrick@gmail.com>
2022-02-21 15:18:57 +0200

    clean code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import { Suspense } from "react";
 import Footer from "./Components/Footer/Footer";
 import Main from "./Components/Main/Main";
 import Nav from "./Components/Nav/Nav";
 
 function App() {
   return (
-    <div>
+    <Suspense
+      fallback={
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      }
+    >
       <Nav />
       <Main />
       <Footer />
-    </div>
+    </Suspense>
   );
 }
 
--- src/i18n.js
+++ src/i18n.js
@@ -1,8 +1,10 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
 
 i18n
+  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
@@ -10,22 +12,6 @@ i18n
     fallbackLng: "en",
     interpolation: {
       escapeValue: false,
-    },
-    resources: {
-      en: {
-        translation: {
-          mainContent: {
-            title: "This is the title",
-          },
-        },
-      },
-      kin: {
-        translation: {
-          mainContent: {
-            title: "uyu ni umutwe",
-          },
-        },
-      },
     },
   });
 
