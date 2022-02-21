29787983ad3b480bb2ca81d18e6597d95598e4be
patricktunez123 <tunezepatrick@gmail.com>
2022-02-21 15:35:13 +0200

    ref

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import "./i18n";
+import "./config/i18n";
 
 ReactDOM.render(
   <React.StrictMode>
